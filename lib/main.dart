1f7dc38212a79dbf56994a33240f60d5db9874c6
shrawani <jawadeshrawani@gmail.com>
2024-02-01 12:20:24 +0530

    new code

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:floating_example/home_page.dart';
+import 'package:floating_example/notificationservice.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationService().initNotification();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notifications',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Local Notifications'),
+    );
+  }
+}
